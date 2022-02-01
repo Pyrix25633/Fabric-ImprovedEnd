9f15d1feefc182ac8ef9dd83ef10e47efc159ccc
Pyrix25633 <biral.mattia@gmail.com>
2022-02-01 09:18:43 +0100

    9 end grass completed

--- src/main/java/net/pyriken/improved_end/block/ModBlocks.java
+++ src/main/java/net/pyriken/improved_end/block/ModBlocks.java
@@ -6,22 +6,38 @@ import net.minecraft.block.Block;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import net.pyriken.improved_end.ImprovedEnd;
+import net.pyriken.improved_end.block.custom.EndGrassBlock;
 import net.pyriken.improved_end.item.ModItemGroup;
 
 public class ModBlocks {
     //Variables to better control block hardness and resistance
     static final float woodHardness = 2f, woodResistance = 2f, stoneHardness = 1.5f, stoneResistance = 2.2f;
+    static final FabricBlockSettings endGrassSettings = FabricBlockSettings.of(Material.SOLID_ORGANIC)
+            .strength(stoneHardness, stoneResistance).sounds(BlockSoundGroup.GRASS).requiresTool();
 
+    //"End Grass"
     public static final Block TIROLIUM = registerBlock("tirolium",
-            new Block(FabricBlockSettings.of(Material.STONE)
-                    .strength(stoneHardness, stoneResistance)
-                    .sounds(BlockSoundGroup.STONE)
-                    .requiresTool()));
+            new EndGrassBlock(endGrassSettings));
+    public static final Block ZIOROLIUM = registerBlock("ziorolium",
+            new EndGrassBlock(endGrassSettings));
+    public static final Block SEROLIUM = registerBlock("serolium",
+            new EndGrassBlock(endGrassSettings));
+    public static final Block TEDOROLIUM = registerBlock("tedorolium",
+            new EndGrassBlock(endGrassSettings));
+    public static final Block GRATOROLIUM = registerBlock("gratorolium",
+            new EndGrassBlock(endGrassSettings));
+    public static final Block PIOROLIUM = registerBlock("piorolium",
+            new EndGrassBlock(endGrassSettings));
+    public static final Block YAMPEROLIUM = registerBlock("yamperolium",
+            new EndGrassBlock(endGrassSettings));
+    public static final Block XIOROLIUM = registerBlock("xiorolium",
+            new EndGrassBlock(endGrassSettings));
+    public static final Block AGGOROLIUM = registerBlock("aggorolium",
+            new EndGrassBlock(endGrassSettings));
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
