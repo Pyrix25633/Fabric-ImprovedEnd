86c9008a10bc76d393a152bbb4d9c5e19243221d
Pyrix25633 <biral.mattia@gmail.com>
2022-08-20 14:01:45 +0200

    1.18.2, Fix

--- src/main/java/net/rupyber_studios/improved_end/ImprovedEndClient.java
+++ src/main/java/net/rupyber_studios/improved_end/ImprovedEndClient.java
@@ -16,7 +16,7 @@ import org.apache.logging.log4j.Logger;
 import software.bernie.geckolib3.GeckoLib;
 
 public class ImprovedEndClient implements ClientModInitializer {
-    public static final Logger LOGGER = LogManager.getLogger("improve_end");
+    public static final Logger LOGGER = LogManager.getLogger("improved_end");
 
     public static final String MOD_ID = "improved_end";
 
